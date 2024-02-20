265fc7076afb436345327f2fb06675527aab4407
kzz <kzz45@qq.com>
2024-02-20 12:35:36 +0800

    node

--- src/views/k8s/corev1/node.vue
+++ src/views/k8s/corev1/node.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="app-container">
     <el-card class="box-card">
-      <el-button type="primary" size="small" icon="el-icon-circle-plus"
+      <!-- <el-button type="primary" size="small" icon="el-icon-circle-plus"
         >新增</el-button
-      >
+      > -->
       <el-table
         :data="page_node_list"
         size="small"
@@ -42,27 +42,51 @@
             }}
           </template>
         </el-table-column>
-        <el-table-column label="操作" width="180px;">
-          <template>
+        <el-table-column label="操作" width="120px;">
+          <template slot-scope="scoped">
             <el-button
               type="primary"
               icon="el-icon-edit"
               size="small"
-              @click="update_node"
+              @click="update_node(scoped.row)"
             ></el-button>
-            <el-tooltip
+            <!-- <el-tooltip
               class="item"
               effect="dark"
-              content="暂停调度"
+              :content="scoped.row.spec.unschedulable ? '启动调度' : '暂停调度'"
               placement="top"
             >
               <el-button
-                type="warning"
-                icon="el-icon-video-pause"
+                :type="scoped.row.spec.unschedulable ? 'success' : 'danger'"
+                :icon="
+                  scoped.row.spec.unschedulable
+                    ? 'el-icon-video-play'
+                    : 'el-icon-video-pause'
+                "
+                size="small"
+                @click="taints_node(scoped.row)"
+              ></el-button>
+            </el-tooltip> -->
+            <el-popconfirm
+              :title="scoped.row.spec.unschedulable ? '启动调度' : '暂停调度'"
+              confirm-button-text="确定"
+              cancel-button-text="不了"
+              style="margin-left: 10px"
+              @confirm="taints_node(scoped.row)"
+              @cancel="cancel_taints"
+            >
+              <el-button
+                slot="reference"
+                :type="scoped.row.spec.unschedulable ? 'success' : 'danger'"
+                :icon="
+                  scoped.row.spec.unschedulable
+                    ? 'el-icon-video-play'
+                    : 'el-icon-video-pause'
+                "
                 size="small"
               ></el-button>
-            </el-tooltip>
-            <el-tooltip
+            </el-popconfirm>
+            <!-- <el-tooltip
               class="item"
               effect="dark"
               content="排空节点"
@@ -73,7 +97,7 @@
                 icon="el-icon-delete"
                 size="small"
               ></el-button>
-            </el-tooltip>
+            </el-tooltip> -->
           </template>
         </el-table-column>
       </el-table>
@@ -86,12 +110,122 @@
         style="text-align: left; margin-top: 20px"
       >
       </el-pagination>
+
+      <el-dialog title="Node" :visible.sync="node_dialog" width="60%">
+        <el-form
+          ref="node_obj_refs"
+          :model="node_obj"
+          size="small"
+          label-width="100px"
+        >
+          <el-tabs v-model="dialog_tabs">
+            <el-tab-pane label="元数据" name="metadata">
+              <MetaDataTpl
+                ref="metadatatpl"
+                :metadata="node_obj.metadata"
+              ></MetaDataTpl>
+            </el-tab-pane>
+            <el-tab-pane label="配置信息" name="spec">
+              <el-form-item label="不可调度" prop="spec.unschedulable">
+                <el-select v-model="node_obj.spec.unschedulable">
+                  <el-option label="是" :value="true"></el-option>
+                  <el-option label="否" :value="false"></el-option>
+                </el-select>
+              </el-form-item>
+              <el-form-item label="污点">
+                <el-button
+                  type="primary"
+                  icon="el-icon-circle-plus"
+                  size="small"
+                  @click="add_node_taint"
+                  >新增</el-button
+                >
+              </el-form-item>
+              <el-form-item label="污点信息" prop="spec.taints">
+                <el-table :data="node_obj.spec.taints" border size="mini">
+                  <el-table-column label="效果" prop="effect">
+                    <template slot-scope="scoped">
+                      <el-input
+                        v-if="scoped.row.isSet"
+                        v-model="scoped.row.effect"
+                        size="mini"
+                      ></el-input>
+                      <el-input
+                        v-else
+                        v-model="scoped.row.effect"
+                        size="mini"
+                        disabled
+                      ></el-input>
+                    </template>
+                  </el-table-column>
+                  <el-table-column label="键" prop="key">
+                    <template slot-scope="scoped">
+                      <el-input
+                        v-if="scoped.row.isSet"
+                        v-model="scoped.row.key"
+                        size="mini"
+                      ></el-input>
+                      <el-input
+                        v-else
+                        v-model="scoped.row.key"
+                        size="mini"
+                        disabled
+                      ></el-input>
+                    </template>
+                  </el-table-column>
+                  <el-table-column label="值" prop="value">
+                    <template slot-scope="scoped">
+                      <el-input
+                        v-if="scoped.row.isSet"
+                        v-model="scoped.row.value"
+                        size="mini"
+                      ></el-input>
+                      <el-input
+                        v-else
+                        v-model="scoped.row.value"
+                        size="mini"
+                        disabled
+                      ></el-input>
+                    </template>
+                  </el-table-column>
+                  <el-table-column label="操作" width="120px">
+                    <template slot-scope="scoped">
+                      <el-button
+                        type="primary"
+                        size="mini"
+                        :icon="
+                          scoped.row.isSet ? 'el-icon-check' : 'el-icon-edit'
+                        "
+                        @click="tains_edit(scoped.row, scoped.row.isSet)"
+                      ></el-button>
+                      <el-button
+                        v-if="!scoped.row.isSet"
+                        type="danger"
+                        icon="el-icon-delete"
+                        size="mini"
+                        @click="tains_del(node_obj.spec.taints, scoped.$index)"
+                      ></el-button>
+                    </template>
+                  </el-table-column>
+                </el-table>
+              </el-form-item>
+            </el-tab-pane>
+          </el-tabs>
+        </el-form>
+        <span slot="footer" class="dialog-footer">
+          <el-button size="small" @click="node_dialog = false">取 消</el-button>
+          <el-button type="primary" size="small" @click="submit_node"
+            >确 定</el-button
+          >
+        </span>
+      </el-dialog>
     </el-card>
   </div>
 </template>
 
 <script>
 import store from "@/store";
+import { cloneDeep } from "lodash";
 import { mapGetters } from "vuex";
 import { parseTime } from "@/utils";
 import {
@@ -114,9 +248,6 @@ const NodeObj = {
     namespace: "",
     annotations: {},
     labels: {},
-    creationTimestamp: {
-      seconds: 0,
-    },
   },
   spec: {
     externalID: "",
@@ -199,8 +330,30 @@ export default {
   },
   data() {
     return {
-      active_tab: "node",
+      textMap: {
+        create_node: "新增",
+        update_node: "编辑",
+      },
+      dialogStatus: "",
       currentPage: 1,
+      node_obj: {
+        metadata: {
+          name: "",
+          namespace: localStorage.getItem("k8s_namespace"),
+          annotations: {},
+          labels: {},
+        },
+        spec: {
+          externalID: "",
+          podCIDR: "",
+          podCIDRs: [],
+          providerID: "",
+          taints: [],
+          unschedulable: false,
+        },
+      },
+      node_dialog: false,
+      dialog_tabs: "metadata",
       node_list: [],
       node_metrics_list: [],
       affinity_list: [],
@@ -208,7 +361,82 @@ export default {
     };
   },
   methods: {
-    update_node(row) {},
+    add_node_taint() {
+      const tempTain = {
+        isSet: false,
+        effect: "NoSchedule",
+        key: "key",
+        value: "",
+      };
+      this.node_obj.spec.taints.push(tempTain);
+    },
+    tains_edit(row, isSet) {
+      row.isSet = !isSet;
+    },
+    tains_del(allData, index) {
+      allData.splice(index, 1);
+    },
+    taints_node(row) {
+      const gvkObj = {
+        group: "core",
+        version: "v1",
+        kind: "Node",
+      };
+      if (row.spec.unschedulable) {
+        let item = cloneDeep(row);
+        item.spec.unschedulable = false;
+        item.spec.taints = [];
+        const param = updateSocketData(gvkObj, item);
+        const ns = localStorage.getItem("k8s_namespace");
+        const updatedata = initSocketData(ns, "core-v1-Node", "update", param);
+        sendSocketMessage(updatedata, store);
+      } else {
+        let item = cloneDeep(row);
+        item.spec.unschedulable = true;
+        item.spec.taints.push({
+          effect: "NoSchedule",
+          key: "node.kubernetes.io/unschedulable",
+          value: "",
+        });
+        const param = updateSocketData(gvkObj, item);
+        const ns = localStorage.getItem("k8s_namespace");
+        const updatedata = initSocketData(ns, "core-v1-Node", "update", param);
+        sendSocketMessage(updatedata, store);
+      }
+    },
+    cancel_taints() {
+      this.$message({
+        type: "warning",
+        message: "你考虑的很全面",
+      });
+    },
+    update_node(row) {
+      this.node_dialog = true;
+      this.dialogStatus = "update_node";
+      this.node_obj = Object.assign({}, row);
+      const temptaint = [];
+      for (const taint of this.node_obj.spec.taints) {
+        const temp = Object.assign({}, taint);
+        temp.isSet = false;
+        temptaint.push(temp);
+      }
+      this.node_obj.spec.taints = temptaint;
+    },
+    submit_node() {
+      if (this.dialogStatus === "update_node") {
+        const gvkObj = {
+          group: "core",
+          version: "v1",
+          kind: "Node",
+        };
+        const item = cloneDeep(this.node_obj);
+        const param = updateSocketData(gvkObj, item);
+        const ns = localStorage.getItem("k8s_namespace");
+        const updatedata = initSocketData(ns, "core-v1-Node", "update", param);
+        sendSocketMessage(updatedata, store);
+        this.node_dialog = false;
+      }
+    },
     get_cpu_mem(node) {
       const nodeInfo = this.node_metrics_list.filter((item) => {
         return (item.metadata.name = node.metadata.name);
